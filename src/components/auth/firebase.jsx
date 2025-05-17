517110c77e643a7653f9c30dd27c1eb518a5e066
Hari-Eshwaran <harishwaranpetchimuthu@gmail.com>
2025-05-17 21:39:59 +0530

    eee

--- src/components/auth/firebase.jsx
+++ src/components/auth/firebase.jsx
@@ -9,14 +9,16 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD4ygAVsbrMzhTqT5JExYYen41pXyWpA4c",
-  authDomain: "centaura-969ff.firebaseapp.com",
-  projectId: "centaura-969ff",
-  storageBucket: "centaura-969ff.firebasestorage.app",
-  messagingSenderId: "885330632715",
-  appId: "1:885330632715:web:d97459d2d70bb163d3a70e",
-  measurementId: "G-J1Z61Z0MHY"
+  apiKey: "AIzaSyChg7bGhqBX_gw9lrGUVIvxdN6cD4PNudM",
+  authDomain: "wealth-map-df0e8.firebaseapp.com",
+  projectId: "wealth-map-df0e8",
+  storageBucket: "wealth-map-df0e8.firebasestorage.app",
+  messagingSenderId: "15224391441",
+  appId: "1:15224391441:web:7f3be36e8bf937ce9b6eb0",
+  measurementId: "G-F69KD0N554"
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
